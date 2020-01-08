f029de7b9ccbd845ed5e265901694ae1890356aa
Byron Wall <byron@byroni.us>
2020-01-07 21:49:46 -0500

    Change line spacing for chapter builds Run build for recent changes

--- bin/create_book_chapters.sh
+++ bin/create_book_chapters.sh
@@ -1,2 +1,2 @@
-for d in ../book/*/ ; do (cd "$d" && PDF=$(echo $d | cut -d'/' -f3-) && pandoc  *.md  -V papersize:letter -o ../../builds/${PDF%?}.pdf --template=../../bin/eisvogel.latex --listings); done
+for d in ../book/*/ ; do (cd "$d" && PDF=$(echo $d | cut -d'/' -f3-) && pandoc  *.md  -V papersize:letter -o ../../builds/${PDF%?}.pdf --template=../../bin/eisvogel_chapters.latex --listings); done
 
