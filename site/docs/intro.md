5aa420659168cc9d810eb8f0bb88bc597b5874a7
Byron Wall <byron@byroni.us>
2021-12-29 23:28:50 -0500

    Minor updates

--- site/docs/intro.md
+++ site/docs/intro.md
@@ -1,1 +1,3 @@
 ## Into
+
+This will contain a list of links to book chapters. And maybe a quick overview of the book's goals. And a link to purchase a PDF?
--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -13,7 +13,14 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}></div>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Read the book
+          </Link>
+        </div>
       </div>
     </header>
   );
